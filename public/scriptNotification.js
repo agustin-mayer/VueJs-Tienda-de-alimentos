0fa2c38e7fd6082c362bfb28f5ee1015890ec512
mayeragustin99 <mayeragustin99@gmail.com>
2023-01-11 03:22:53 -0300

    Revert "probando notificacion"
    This reverts commit 0969e0a2b544db248155b21bd691f705392dd44c.

--- public/scriptNotification.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector("button")
-
-button.addEventListener("click", () => {
-    Notification.requestPermission().then(perm => {
-        if (perm === "granted") {
-            alert(new Notification("¡Las cervezas estan en oferta!"))
-        }
-    })
-})
